43a80170b03b34303e198fe108a4a25a72fb5448
AvigyanChowdhury <146307887+AvigyanChowdhury@users.noreply.github.com>
2023-10-02 18:51:57 +0530

    Delete whisper_ecapa_tdnn/whisper_ecapa_tdnn directory

--- whisper_ecapa_tdnn/whisper_ecapa_tdnn/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-
-import subprocess
-
-# First line of code
-subprocess.run(["python", "model.py"])
-
-# change reference rttm file
-subprocess.run(["python", "evaluation.py", "-r", "aepyx.rttm", "-s", "aepyx_hyp.rttm"])
